7e5025a376f6d3f3095b6fa10d3035e32cc7d365
Nicholas Lexi <lexi.nicholas03@gmail.com>
2022-10-28 15:46:58 +0700

    fixing modal

--- hotel/views.py
+++ hotel/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from hotel.models import *
 from django.contrib.auth.decorators import login_required
-from django.contrib import messages
 from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from hotel.forms import *
 from django.urls import reverse

abe8dd06a3bd979d4b115a727388c95bf9003d02
Mohammad Attar <mohammad.attar.w@gmail.com>
2022-11-02 23:11:04 +0700

    Added new about app

--- about/models.py
+++ about/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class UserSuggestion(models.Model):
+    full_name = models.CharField(max_length=50, null=True, blank=True),
+    phone_number = models.CharField(max_length=15, null=True, blank=True),
+    email = models.CharField(max_length=50, null=True, blank=True)
+    message = models.TextField()
+    date = models.DateField(auto_now=True)
--- about/views.py
+++ about/views.py
@@ -1,5 +1,37 @@
 from django.shortcuts import render
+from django.http.response import JsonResponse
+from about.models import UserSuggestion
+from django.http import HttpResponseBadRequest
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
-def launch(request):
-    return render(request, 'about.html')
+def show_about_section(request):
+    return render(
+        request, 
+        'about.html',
+        {}
+    )
+
+@csrf_exempt
+def post_suggestion(request):
+    if request.method == 'POST':
+        full_name = request.POST.get('full_name')
+        phone_number = request.POST.get('phone_number')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+        print(full_name)
+        if full_name is not None and phone_number is not None and email is not None:
+            UserSuggestion.objects.create(
+                full_name=full_name,
+                phone_number=phone_number,
+                email=email,
+                message=message
+            )
+
+            return JsonResponse({
+                'name': full_name,
+                'status': 'success',
+                'error': False
+            })
+
+    return HttpResponseBadRequest('Bad Request')

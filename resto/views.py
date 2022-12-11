892df6edbc73d01d9b1fec52ea9d82256cf9c9bf
Kaloosh F. Verrell <kf.verrell@gmail.com>
2022-12-11 17:57:11 +0700

    edit delete resto flutter back to url

--- resto/views.py
+++ resto/views.py
@@ -72,8 +72,7 @@ def create_restaurant_flutter(request):
         resto.save()
         return HttpResponse(status=201)
 
-def delete_restaurant_flutter(request):
-    id = int(request.POST.get("id"))
+def delete_restaurant_flutter(request, id):
     object = get_object_or_404(Restaurant, pk = id) 
     object.delete()
     return HttpResponse(status=204)

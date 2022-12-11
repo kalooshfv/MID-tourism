10a0f2ddbc787106d8df7ef4b865bd7e3a499eb3
Kaloosh F. Verrell <kf.verrell@gmail.com>
2022-12-11 17:26:55 +0700

    edit id to int

--- resto/views.py
+++ resto/views.py
@@ -73,7 +73,7 @@ def create_restaurant_flutter(request):
         return HttpResponse(status=201)
 
 def delete_restaurant_flutter(request):
-    id = request.POST.get("id")
+    id = int(request.POST.get("id"))
     object = get_object_or_404(Restaurant, pk = id) 
     object.delete()
     return HttpResponse(status=204)
